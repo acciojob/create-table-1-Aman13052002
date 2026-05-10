de022c46ac15e1da3a08e70d8a4348854a7cd499
Aman Kumar <125535132+Aman13052002@users.noreply.github.com>
2026-05-10 18:29:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function insert_Row() {
     //Write your code here
-  
-  
+  const table = document.querySelector('#sampleTable');
+ //  const insert = document.querySelector('input[type="button"]');
+	// insert.addEventListener('click',function(){
+	// 	const newRow = document.createElement('tr');
+	// 	newRow.innerHTML = `<td>New Cell1</td><td>New Cell2</td>`;
+	// 	table.appendChild(newRow);
+	// })
+	const newRow = table.insertRow(0);
+	const cell1 = newRow.insertCell(0);
+	const cell2 = newRow.insertCell(1);
+	cell1.textContent = 'New Cell1';
+	cell2.textContent = 'New Cell2';
 }
